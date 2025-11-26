39cb3379f483436141053a6b7747b1f33a4ee463
Raman Preet Singh <ramanjrpreetsingh9889@gmail.com>
2025-11-27 03:47:17 +0530

    style changed

--- script.js
+++ script.js
@@ -1,55 +1,35 @@
 const hamburger = document.querySelector('.hamburger');
 const navMenu = document.querySelector('.nav-menu');
+const header = document.querySelector('.header');
 const navbar = document.querySelector('.navbar');
 const main = document.querySelector('.main');
 const description = document.querySelector('.description');
+const resultSection = document.querySelector('#results')
+const headerHeight = document.querySelector('.header').offsetHeight;
 
 let isActive = false
 
-hamburger.addEventListener('click', ()=>{
+hamburger.addEventListener('click', () => {
   isActive = !isActive
   hamburger.classList.toggle('active')
-  navbar.classList.toggle(isActive ? 'expand' : 'collapse');
-  navbar.classList.remove(isActive ? 'collapse' : 'expand');
-  navMenu.classList.toggle(isActive ? 'active' : 'inactive');
-  navMenu.classList.remove(isActive ? 'inactive' : 'active');
-  // main.classList.toggle(isActive ? 'down' : 'up');
-  // main.classList.remove(isActive ? 'up' : 'down');
-
+  header.classList.toggle('expand');
 })
 
-document.querySelectorAll('.nav-link').forEach(n => n.addEventListener('click', ()=>{
-  hamburger.classList.remove('active');
-  isActive = !isActive
-  navbar.classList.toggle(isActive ? 'expand' : 'collapse');
-  navbar.classList.remove(isActive ? 'collapse' : 'expand');
-  navMenu.classList.toggle(isActive ? 'active' : 'inactive');
-  navMenu.classList.remove(isActive ? 'inactive' : 'active');
-  // main.classList.toggle(isActive ? 'down' : 'up');
-  // main.classList.remove(isActive ? 'up' : 'down');
-}))
-
-window.addEventListener('scroll', function() {
-  let header = document.querySelector('header');
-  let bar = document.querySelectorAll('span');
-  let text = document.querySelectorAll('.text')
-  let navLink = document.querySelectorAll('.nav-link');
-  let toTop = document.getElementById('upward');
-  header.classList.toggle('sticky', window.scrollY > 0);
-  bar.forEach(x => x.classList.toggle('sticky', window.scrollY > 0));
-  navMenu.classList.toggle('darken', window.scrollY > 0);
-  main.classList.toggle('darken', window.scrollY > 0);
-  text.forEach(x => x.classList.toggle('lighten', window.scrollY > 0));
-  navLink.forEach(x => x.classList.toggle('darken', window.scrollY > 0));
-  toTop.classList.toggle('visible', window.scrollY > 0);
-
-  
+window.addEventListener('scroll', function () {
+  header.classList.toggle('active', window.scrollY > 0);
 });
 
-function gotoTop(){
-  window.scrollTo(0,0)
+function gotoTop() {
+  hamburger.classList.remove('active')
+  header.classList.remove('expand');
+  window.scrollTo(0, 0)
 }
 
-function gotoResults(){
-  window.scrollTo(0,425)
+function gotoResults() {
+  hamburger.classList.remove('active')
+  header.classList.remove('expand');
+  window.scrollTo({
+  top: resultSection.offsetTop - headerHeight,
+  behavior: 'smooth'
+});
 }
